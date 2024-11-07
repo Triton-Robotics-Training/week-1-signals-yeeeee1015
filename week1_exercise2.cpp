0407e03b08cd648ae0d8dba413ff0bd5fb4bbef2
yeeeee1015 <eric.ye04@yahoo.com>
2024-11-07 00:41:45 -0800

    week 1 training

--- week1_exercise2.cpp
+++ week1_exercise2.cpp
@@ -4,11 +4,15 @@
 //IF YOU HAVE ISSUES WITH THE SIMULATOR, CONTACT EMBEDDED LEAD
 #include "mbed.h"
 
+AnalogIn pot(p15);
+DigitalOut LED(LED1);
+
 int main() {
     while (1) {
-        printf("Week 1 Exercise 2");
-
-        // MAKE SURE THERE IS ALWAYS A WAIT ON THE SIM OR IT WILL CRASH
-        wait_ms(500); 
+        double x = pot.read();
+        LED = 1;
+        wait_ms(2000*x);
+        LED = 0;
+        wait_ms(2000*(1-x));
     }
 }
